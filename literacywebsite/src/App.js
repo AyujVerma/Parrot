717b30f3712c0fd216bcfab9c0d6eab0e1803b06
rakshagirl <68088408+rakshagirl@users.noreply.github.com>
2023-03-18 10:21:44 -0500

    fixed database initialization

--- literacywebsite/src/App.js
+++ literacywebsite/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+
 // importing components from react-router-dom package
 import {
   BrowserRouter as Router,
@@ -17,6 +18,7 @@ import Writing from "./components/Writing";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
